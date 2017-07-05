82328fe80f2d391c0a422de7a378a5341e280f61
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-07-05 14:21:20 -0700

    Add neighborhood_map.html file and update new map lat/long for mountain view area

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ function loadData() {
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
+    var $wuElem = $('#weather-underground-forecast');
     var $greeting = $('#greeting');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
+    $wuElem.text("");
 
     // load streetview
 
@@ -22,6 +24,20 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src = " ' + streetviewUrl + '">');
 
+    // weather underground AJAX request goes here
+    var weatherundergroundUrl = "http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"
+
+    jQuery(document).ready(function($) {
+      $.ajax({
+      url : "http://api.wunderground.com/api/65646c8c86b0eefe/conditions/q/CA/San_Francisco.json",
+      dataType : "jsonp",
+      success : function(parsed_json) {
+      var location = parsed_json['location']['city'];
+      var temp_f = parsed_json['current_observation']['temp_f'];
+      alert("Current temperature in " + location + " is: " + temp_f);
+      }
+      });
+    });
 
     // nytimes AJAX request goes here
 
