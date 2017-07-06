803c9bdccd4f68567bc8a0db70169dfa33ab2b53
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-07-05 18:52:56 -0700

    Update add weather under group api and adjust css positions

--- js/script.js
+++ js/script.js
@@ -5,39 +5,43 @@ function loadData() {
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
+    var $wuHeaderElem = $('#weather-underground-header');
     var $wuElem = $('#weather-underground-forecast');
-    var $greeting = $('#greeting');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
     $wuElem.text("");
 
-    // load streetview
 
+    // load streetview
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
+    var stateStr = $('#state').val();
+    var address = streetStr + ', ' + cityStr + ', ' + stateStr;
 
-    $greeting.text('So, you want to live at ' + address + '?');
 
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src = " ' + streetviewUrl + '">');
 
+
     // weather underground AJAX request goes here
-    var weatherundergroundUrl = "http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"
+    var weatherundergroundUrl = "http://api.wunderground.com/api/65646c8c86b0eefe/forecast/q/" + stateStr  + "/" + cityStr + ".json"
 
     jQuery(document).ready(function($) {
+
+      $wuHeaderElem.text('Current Weather in ' + cityStr);
+
       $.ajax({
-      url : "http://api.wunderground.com/api/65646c8c86b0eefe/conditions/q/CA/San_Francisco.json",
+      url : weatherundergroundUrl,
       dataType : "jsonp",
-      success : function(parsed_json) {
-      var location = parsed_json['location']['city'];
-      var temp_f = parsed_json['current_observation']['temp_f'];
+      success : function(url) {
+      var location = url[stateStr][cityStr];
+      var temp_f = url['current_observation']['temp_f'];
       alert("Current temperature in " + location + " is: " + temp_f);
       }
-      });
-    });
+  });
+});
 
     // nytimes AJAX request goes here
 
@@ -83,6 +87,7 @@ function loadData() {
         }
     });
     return false;
+
 }
 
 $('#form-container').submit(loadData);
