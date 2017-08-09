4b451dd41d673250dcdc91065b5273c65cde8a5c
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-08-09 14:39:26 -0700

    Update remove unused images

--- js/script.js
+++ js/script.js
@@ -87,24 +87,6 @@ function loadData() {
     });
     return false;
 
-    // jQuery for star rating in places-search
-    // source:  https://stackoverflow.com/questions/1987524/turn-a-number-into-star-rating-display-using-jquery-and-css
-    $.fn.stars = function() {
-        return $(this).each(function() {
-            // Get the value
-            val = Math.round(val * 4) / 4; /* To round to nearest quarter */
-            val = Math.round(val * 2) / 2; /* To round to nearest half */
-            var val = parseFloat($(this).html());
-            // Make sure that the value is in 0 - 5 range, multiply to get width
-            var size = Math.max(0, (Math.min(5, val))) * 16;
-            // Create stars holder
-            var $span = $('<span />').width(size);
-            // Replace the numerical value with stars
-            $(this).html($span);
-        });
-    }
-
-
 }
 
 $('#form-container').submit(loadData);
