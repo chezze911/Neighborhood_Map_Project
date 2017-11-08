d14b0c8018d26dbd6da385605eda1740b5429524
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-11-07 17:04:22 -0800

    Update Asynchronous data usage using async attribute and oneerror attribute for providing a fallback error handling method

--- js/script.js
+++ js/script.js
@@ -300,6 +300,25 @@
       marker.addListener('mouseout', function() {
         this.setIcon(defaultIcon);
       });
+      mapError = function (msg, url, lineNo, columnNo, error) {
+          var string = msg.toLowerCase();
+          var substring = "script error";
+          if (string.indexOf(substring) > -1){
+              alert('Script Error: See Browser Console for Detail');
+          } else {
+              var message = [
+                  'Message: ' + msg,
+                  'URL: ' + url,
+                  'Line: ' + lineNo,
+                  'Column: ' + columnNo,
+                  'Error object: ' + JSON.stringify(error)
+              ].join(' - ');
+
+              alert(message);
+          }
+
+          return false;
+      };
       
     }
     ko.applyBindings(new ViewModel());
