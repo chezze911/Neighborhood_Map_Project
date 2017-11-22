de67f1725c51d125a550c1544264568bdb708c35
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-11-21 19:50:43 -0800

    Separate defaultLocations son data into a separate .json file

--- js/script.js
+++ js/script.js
@@ -36,14 +36,14 @@ var map;
 var markers = [];
 
 
-var defaultLocations = [
-    { name: 'Pho Y #1', position: { lat: 37.305973, lng: -121.812203 }, id: "4ac8f48ff964a52092bd20e3" },
-    { name: 'Khanh\'s', position: { lat: 37.321959, lng: -121.950478 }, id: "4a4149d1f964a5200aa51fe3" },
-    { name: 'Vung Tau Restaurant', position: { lat: 37.341639, lng: -121.879504 }, id: "4aabfe9df964a520735b20e3" },
-    { name: 'Huong Lan Sandwich', position: { lat: 37.321542, lng: -121.827426 }, id: "4b37b9f3f964a520174525e3" },
-    { name: 'Gio Cha Duc Huong Sandwich', position: { lat: 37.331328, lng: -121.855022 }, id: "53b739f0498e1b9ab96408aa" },
-    { name: 'Bun Bo Hue An Nam', position: { lat: 37.328845, lng: -121.858199 }, id: "4b2d9bdff964a52081d924e3" }
-];
+// var defaultLocations = [
+//     { name: 'Pho Y #1', position: { lat: 37.305973, lng: -121.812203 }, id: "4ac8f48ff964a52092bd20e3" },
+//     { name: 'Khanh\'s', position: { lat: 37.321959, lng: -121.950478 }, id: "4a4149d1f964a5200aa51fe3" },
+//     { name: 'Vung Tau Restaurant', position: { lat: 37.341639, lng: -121.879504 }, id: "4aabfe9df964a520735b20e3" },
+//     { name: 'Huong Lan Sandwich', position: { lat: 37.321542, lng: -121.827426 }, id: "4b37b9f3f964a520174525e3" },
+//     { name: 'Gio Cha Duc Huong Sandwich', position: { lat: 37.331328, lng: -121.855022 }, id: "53b739f0498e1b9ab96408aa" },
+//     { name: 'Bun Bo Hue An Nam', position: { lat: 37.328845, lng: -121.858199 }, id: "4b2d9bdff964a52081d924e3" }
+// ];
 
 
 var menu = document.querySelector('#menu');
