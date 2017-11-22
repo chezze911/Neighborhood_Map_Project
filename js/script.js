0b46491d584da62c25e529837678699d3c612108
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-11-21 19:53:04 -0800

    Move mapError function out to the global scope

--- js/script.js
+++ js/script.js
@@ -36,16 +36,6 @@ var map;
 var markers = [];
 
 
-// var defaultLocations = [
-//     { name: 'Pho Y #1', position: { lat: 37.305973, lng: -121.812203 }, id: "4ac8f48ff964a52092bd20e3" },
-//     { name: 'Khanh\'s', position: { lat: 37.321959, lng: -121.950478 }, id: "4a4149d1f964a5200aa51fe3" },
-//     { name: 'Vung Tau Restaurant', position: { lat: 37.341639, lng: -121.879504 }, id: "4aabfe9df964a520735b20e3" },
-//     { name: 'Huong Lan Sandwich', position: { lat: 37.321542, lng: -121.827426 }, id: "4b37b9f3f964a520174525e3" },
-//     { name: 'Gio Cha Duc Huong Sandwich', position: { lat: 37.331328, lng: -121.855022 }, id: "53b739f0498e1b9ab96408aa" },
-//     { name: 'Bun Bo Hue An Nam', position: { lat: 37.328845, lng: -121.858199 }, id: "4b2d9bdff964a52081d924e3" }
-// ];
-
-
 var menu = document.querySelector('#menu');
 var main = document.querySelector('#map');
 var drawer = document.querySelector('#drawer');
@@ -271,7 +261,13 @@ function initMap() {
         marker.addListener('mouseout', function () {
             this.setIcon(defaultIcon);
         });
-        mapError = function (msg, url, lineNo, columnNo, error) {
+
+    }
+    ko.applyBindings(new ViewModel());
+
+}
+
+mapError = function (msg, url, lineNo, columnNo, error) {
             var string = msg.toLowerCase();
             var substring = "script error";
             if (string.indexOf(substring) > -1) {
@@ -291,10 +287,6 @@ function initMap() {
             return false;
         };
 
-    }
-    ko.applyBindings(new ViewModel());
-
-}
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
